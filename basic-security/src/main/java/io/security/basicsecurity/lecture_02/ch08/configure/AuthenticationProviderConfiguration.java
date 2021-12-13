a90849db50639b34e38a21275fbeea3e50bbea4f
Bob Park <hwpark11@kakao.com>
2021-12-13 22:06:25 +0900

    feat : 인증 처리자 - AuthenticationProvider

---
+++ basic-security/src/main/java/io/security/basicsecurity/lecture_02/ch08/configure/AuthenticationProviderConfiguration.java
@@ -0,0 +1,12 @@
+package io.security.basicsecurity.lecture_02.ch08.configure;
+
+/**
+ * AuthenticationProvider
+ *
+ * <pre>
+ *     - 실질적으로 인증처리를 하는 역할
+ * </pre>
+ */
+public class AuthenticationProviderConfiguration {
+
+}
