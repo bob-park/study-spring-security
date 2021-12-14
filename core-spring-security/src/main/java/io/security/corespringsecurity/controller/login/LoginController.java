03ca223a24e30de489c034951b8431695d18805a
Bob Park <hwpark11@kakao.com>
2021-12-14 22:37:04 +0900

    feat : 로그아웃 및 인증에 따른 화면 보안 처리

--- core-spring-security/src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ core-spring-security/src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,5 +1,9 @@
 package io.security.corespringsecurity.controller.login;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,5 +13,16 @@ public class LoginController {
     @GetMapping(path = "login")
     public String login() {
         return "user/login/login";
+    }
+
+    @GetMapping(path = "logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) {
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/";
     }
 }
