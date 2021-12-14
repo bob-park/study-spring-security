3c4c0f4e04796ec585e46b44bc88cf73941a158a
Bob Park <hwpark11@kakao.com>
2021-12-14 21:18:03 +0900

    feat : 정적 자원 관리 - WebIgnore 설정

--- core-spring-security/src/main/java/io/security/corespringsecurity/security/configure/SecurityConfiguration.java
+++ core-spring-security/src/main/java/io/security/corespringsecurity/security/configure/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package io.security.corespringsecurity.security.configure;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -13,17 +15,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    /**
+     * WebIgnore 설정
+     *
+     * <pre>
+     *     - js / css / image 파일 등 보안 필터를 적용할 필요가 없는 리소스를 설정
+     * </pre>
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         String password = passwordEncoder().encode("12345");
 
+        // 추후 권한 계층으로 할것임
         auth.inMemoryAuthentication()
             .withUser("user").password(password).roles("USER")
             .and()
-            .withUser("manager").password(password).roles("MANAGER")
+            .withUser("manager").password(password).roles("MANAGER", "USER")
             .and()
-            .withUser("admin").password(password).roles("ADMIN")
+            .withUser("admin").password(password).roles("ADMIN", "MANAGER", "USER")
         ;
     }
 
